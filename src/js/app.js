5a86032fb3bca8bd72b7acb35496e1a7424e75ed
Helen <gu-helen04@mail.ru>
2024-02-24 03:17:35 +0500

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,10 @@
-import userLife from './task1';
+//import userLife from './task1';
 
-const user = [
-  { name: 'алхимик', health: 40 },
-  { name: 'маг', health: 100 },
-  { name: 'некромант', health: 80 },
-  { name: 'ведьма', health: 60 },
-];
+//const user = [
+  //{ name: 'алхимик', health: 40 },
+  //{ name: 'маг', health: 100 },
+  //{ name: 'некромант', health: 80 },
+  //{ name: 'ведьма', health: 60 },
+//];
 
-matchers(user);
+//matchers(user);
